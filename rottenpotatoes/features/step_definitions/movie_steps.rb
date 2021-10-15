f80d3d165f818511a3806d68d03cc8244c3b6c4c
Hanyi Li <hl3512@columbia.edu>
2021-10-15 02:09:33 +0000

    complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,7 +2,7 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-      Movie.create(movie)
+      Movie.create!(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
@@ -17,7 +17,7 @@ end
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-    expect(page).to have_content("{#e1.+#e2}")
+    expect(page).to have_content(~ /#{e1}.+#{e2}/m)
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
   # fail "Unimplemented"
